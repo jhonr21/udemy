19299bae6f36e95cccde8411e5fd7660073e0926
Jhon Rangel <jhonrangel21@gmail.com>
2021-01-06 23:14:49 -0400

    servidor

---
+++ udemy/web-personal/cliente/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Te amo ana</h1>
+      <h2>Te Casas conmigo?</h2>
+
+    </div>
+  );
+}
+
+export default App;
--- web-personal/cliente/src/App.js
+++ web-personal/cliente/src/App.js
@@ -1,11 +1,23 @@
-import logo from './logo.svg';
+import {DatePicker, Card} from 'antd';
 import './App.scss';
 
 function App() {
+  const test = (date, dateString) =>{
+    console.log(date, dateString)
+  }
   return (
     <div className="app">
       <h1>Te amo ana</h1>
       <h2>Te Casas conmigo?</h2>
+
+      
+      <DatePicker onChange={test} />
+
+      <Card title="Default size card" extra={<a href="#">More</a>} style={{ width: 300 }}>
+      <p>Card content</p>
+      <p>Card content</p>
+      <p>Card content</p>
+    </Card>
 
     </div>
   );
